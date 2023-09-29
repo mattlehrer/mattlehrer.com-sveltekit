e25d24000ef2b12329a28cef5e565d3e7f084650
Matt Lehrer <lehrerm@gmail.com>
2023-09-29 13:49:47 +0200

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,23 @@
 <script>
+	import Header from '$lib/layout/Header.svelte';
+	import Footer from '$lib/layout/Footer.svelte';
+
 	import '../app.css';
 </script>
 
-<slot />
+<div class="7xl:px-0 mx-auto grid max-w-7xl px-8">
+	<Header />
+
+	<main class="py-8 sm:py-12">
+		<slot />
+	</main>
+
+	<Footer />
+</div>
+
+<style>
+	div {
+		min-height: 100dvh;
+		grid-template-rows: auto 1fr auto;
+	}
+</style>
