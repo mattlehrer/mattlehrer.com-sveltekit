2ed0083d0f5e27f9126d98cf7ef00ee423725340
Matt Lehrer <lehrerm@gmail.com>
2023-10-05 14:58:24 +0200

    webfinger

---
+++ src/routes/.well-known/webfinger/+server.ts
@@ -0,0 +1,34 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async () => {
+	return json(
+		{
+			subject: 'acct:mattlehrer@definitely.social',
+			aliases: [
+				'https://definitely.social/@mattlehrer',
+				'https://definitely.social/users/mattlehrer',
+			],
+			links: [
+				{
+					rel: 'http://webfinger.net/rel/profile-page',
+					type: 'text/html',
+					href: 'https://definitely.social/@mattlehrer',
+				},
+				{
+					rel: 'self',
+					type: 'application/activity+json',
+					href: 'https://definitely.social/users/mattlehrer',
+				},
+				{
+					rel: 'http://ostatus.org/schema/1.0/subscribe',
+					template: 'https://definitely.social/authorize_interaction?uri={uri}',
+				},
+			],
+		},
+		{
+			headers: {
+				'content-type': 'application/jrd+json',
+			},
+		},
+	);
+};
