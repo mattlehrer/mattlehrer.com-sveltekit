5ca7339623520994ca7d4885651ca562f5f8575f
Matt Lehrer <lehrerm@gmail.com>
2023-10-06 13:35:42 +0200

    remove max width too narrow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,7 @@
 	<ul class="grid divide-y divide-primary-700">
 		{#each data.posts.slice(0, MAX_POSTS) as post}
 			<li>
-				<div class="max-w-lg">
+				<div>
 					<a href={'/blog/' + post.slug} class="text-lg font-semibold capitalize">{post.title}</a>
 					<p class="font-extralight text-primary-900">{formatDate(post.date)}</p>
 					{#if post.description}
