36d69c41433482f4128f47132112cd5984d4774a
Matt Lehrer <lehrerm@gmail.com>
2023-10-08 23:06:42 +0200

    error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="error grid h-full place-content-center text-4xl">
+	<h1>{$page.status}: <span class="font-bold">{$page.error?.message}</span></h1>
+</div>
