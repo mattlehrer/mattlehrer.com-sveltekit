2d6afdc18ab4fe9d30672d40989c22a7413df0d5
Matt Lehrer <lehrerm@gmail.com>
2023-10-30 15:02:58 +0100

    don't prerender

--- src/routes/og/+page.ts
+++ src/routes/og/+page.ts
@@ -1,5 +1,7 @@
 import type { PageLoad } from './$types';
 
+export const prerender = false;
+
 export const load: PageLoad = ({ url }) => {
 	return {
 		title: url.searchParams.get('title') ?? 'Hello, world!',
