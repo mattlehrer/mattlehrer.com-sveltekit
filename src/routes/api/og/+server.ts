6ab84d92209f85181582858506a0f69426e41594
Matt Lehrer <lehrerm@gmail.com>
2023-10-31 14:06:49 +0100

    test

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -5,25 +5,21 @@ const width = 1200;
 const height = 630;
 
 export const GET: RequestHandler = async ({ platform, url }) => {
-	const title = url.searchParams.get('title') ?? undefined;
+	console.log({ platform });
 
-	if (!title) {
-		return new Response('Missing title', { status: 400 });
-	} else if (!platform?.env.MYBROWSER) {
-		return new Response('Missing MYBROWSER environment variable', { status: 500 });
-	} else {
-		const browser = await puppeteer.launch(platform.env.MYBROWSER);
-		const page = await browser.newPage();
-		await page.setViewport({ width, height, deviceScaleFactor: 1.5 });
+	const title = url.searchParams.get('title') ?? '';
 
-		await page.goto(url + '/api/og/render?title=' + title);
-		const img = (await page.screenshot({ type: 'png' })) as Buffer;
-		await browser.close();
+	const browser = await puppeteer.launch(platform.env.MYBROWSER);
+	const page = await browser.newPage();
+	await page.setViewport({ width, height, deviceScaleFactor: 1.5 });
 
-		return new Response(img, {
-			headers: {
-				'content-type': 'image/png',
-			},
-		});
-	}
+	await page.goto(url + '/api/og/render?title=' + title);
+	const img = (await page.screenshot({ type: 'png' })) as Buffer;
+	await browser.close();
+
+	return new Response(img, {
+		headers: {
+			'content-type': 'image/png',
+		},
+	});
 };
