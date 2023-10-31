a3ec0e140f0f380513e6fdcd59fde86f7fcb5c76
Matt Lehrer <lehrerm@gmail.com>
2023-10-31 22:28:11 +0100

    test vercel/og

--- src/routes/api/og/+server.ts
+++ src/routes/api/og/+server.ts
@@ -1,25 +1,32 @@
-import puppeteer from '@cloudflare/puppeteer';
+import { ImageResponse } from '@cloudflare/pages-plugin-vercel-og/api';
+import Card from '$lib/components/og/OGCard.svelte';
 import type { RequestHandler } from './$types';
+import { html as toReactNode } from 'satori-html';
+import type { SvelteComponent } from 'svelte';
+import PlusJakartaSans from '$lib/components/og/PlusJakartaSans-Regular.ttf';
+
+export const prerender = false;
 
 const width = 1200;
 const height = 630;
 
-export const GET: RequestHandler = async ({ platform, url }) => {
-	console.log({ platform });
+export const GET: RequestHandler = async ({ url }) => {
+	const title = url.searchParams.get('title') ?? undefined;
+	const result = (Card as unknown as SvelteComponent).render({ title });
+	const element = toReactNode(`${result.html}<style>${result.css.code}</style>`);
 
-	const title = url.searchParams.get('title') ?? '';
-
-	const browser = await puppeteer.launch(platform.env.MYBROWSER);
-	const page = await browser.newPage();
-	await page.setViewport({ width, height, deviceScaleFactor: 1.5 });
-
-	await page.goto(url + '/api/og/render?title=' + title);
-	const img = (await page.screenshot({ type: 'png' })) as Buffer;
-	await browser.close();
-
-	return new Response(img, {
+	return new ImageResponse(element, {
+		fonts: [
+			{
+				name: 'Plus Jakarta Sans',
+				data: Buffer.from(PlusJakartaSans),
+				style: 'normal',
+			},
+		],
+		width: width,
+		height: height,
 		headers: {
 			'content-type': 'image/png',
 		},
-	});
+	}) as ReturnType<RequestHandler>;
 };
