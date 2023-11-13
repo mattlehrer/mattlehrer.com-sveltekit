c8b097b9e286fe113fd00d49ce193d1609308f26
Matt Lehrer <lehrerm@gmail.com>
2023-11-13 14:15:49 +0100

    add links to podcast recommendations page

--- src/lib/layout/Header.svelte
+++ src/lib/layout/Header.svelte
@@ -1,30 +1,128 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+
+	let isChecked = false;
 </script>
 
-<header class="full-bleed px-8 sm:px-16">
+<header class="full-bleed px-8 text-primary-800 sm:px-16">
 	<nav class="flex items-center justify-between py-8">
 		{#if $page.route.id !== '/'}
 			<a
-				class="pt-1 text-inherit no-underline focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-primary-700"
+				class="pt-1 text-inherit no-underline focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-primary-800"
 				href="/"
 			>
-				<span class="fancy-serif-bold text-3xl font-black sm:text-5xl">ML</span>
+				<span class="fancy-serif-bold text-4xl font-black md:text-5xl">ML</span>
 			</a>
-			<div class="flex gap-2 text-sm sm:gap-6 sm:text-base">
-				<a
-					class="px-2 hover:scale-110 hover:text-primary-800 focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
-					href="/about">About</a
+			<div class="flex items-center justify-between">
+				<input
+					id="menu-toggle"
+					class="absolute cursor-pointer opacity-0"
+					type="checkbox"
+					bind:checked={isChecked}
+				/>
+				<label
+					class="menu-button-container z-10 flex h-full w-12 cursor-pointer flex-col items-center justify-center md:hidden"
+					for="menu-toggle"
 				>
-				<a
-					class="px-2 hover:scale-110 hover:text-primary-800 focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
-					href="/blog">Blog</a
+					<div class="menu-button"></div>
+				</label>
+				<ul
+					class="menu absolute left-0 top-0 z-20 mt-24 flex w-full list-none flex-col items-center justify-center text-sm md:relative md:mt-0 md:w-auto md:flex-row md:gap-4 md:text-base"
 				>
-				<a
-					class="px-2 hover:scale-110 hover:text-primary-800 focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
-					href="/reading/books">Book reviews</a
-				>
+					<li>
+						<a on:click={() => (isChecked = false)} href="/about">About</a>
+					</li>
+					<li>
+						<a on:click={() => (isChecked = false)} href="/blog">Blog</a>
+					</li>
+					<li>
+						<a on:click={() => (isChecked = false)} href="/reading/books">Book reviews</a>
+					</li>
+					<li>
+						<a on:click={() => (isChecked = false)} href="/podcast-listening"
+							>Podcast recommendations</a
+						>
+					</li>
+				</ul>
 			</div>
 		{/if}
 	</nav>
 </header>
+
+<style lang="postcss">
+	li {
+		@apply m-0 h-0 md:h-auto;
+	}
+
+	li > a {
+		@apply px-2 text-lg hover:scale-110 focus-visible:rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-800 md:text-base lg:text-lg;
+	}
+
+	.menu-button,
+	.menu-button::before,
+	.menu-button::after {
+		/* display: block; */
+		background-color: theme(colors.primary.700);
+		position: absolute;
+		height: 4px;
+		width: 40px;
+		transition: transform 400ms cubic-bezier(0.23, 1, 0.32, 1);
+		border-radius: 2px;
+	}
+
+	.menu-button::before {
+		content: '';
+		margin-top: -10px;
+	}
+
+	.menu-button::after {
+		content: '';
+		margin-top: 10px;
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button::before {
+		margin-top: 0px;
+		transform: rotate(405deg);
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button {
+		background: rgba(255, 255, 255, 0);
+	}
+
+	#menu-toggle:checked + .menu-button-container .menu-button::after {
+		margin-top: 0px;
+		transform: rotate(-405deg);
+	}
+
+	@media (max-width: theme(screens.md)) {
+		#menu-toggle ~ .menu li {
+			padding: 0;
+			border: 0;
+			transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+			visibility: hidden;
+		}
+
+		#menu-toggle:checked ~ .menu li {
+			visibility: visible;
+			border: 1px solid theme(colors.primary.800);
+			height: auto;
+			padding: 1em;
+			transition: height 400ms cubic-bezier(0.23, 1, 0.32, 1);
+		}
+		.menu > li {
+			display: flex;
+			justify-content: center;
+			width: 100%;
+			color: theme(colors.primary.100);
+			background-color: theme(colors.primary.700);
+		}
+
+		.menu > li:hover {
+			background-color: theme(colors.primary.800);
+		}
+
+		#menu-toggle:focus-visible ~ .menu-button-container {
+			@apply h-10 w-12 rounded outline outline-2 outline-offset-2 outline-primary-700;
+		}
+	}
+</style>
--- src/lib/layout/Hero.svelte
+++ src/lib/layout/Hero.svelte
@@ -1,6 +1,6 @@
 <section id="hero" class="wrapper" aria-labelledby="hero-heading">
 	<div class="relative isolate py-12">
-		<div class=" max-w-2xl py-12 lg:py-32">
+		<div class=" max-w-3xl py-12 lg:py-32">
 			<div class="fancy-serif-regular">
 				<h1 id="hero-heading" class="text-4xl tracking-tight sm:text-6xl">
 					<span class="opacity-90">Hi, I'm </span><span class="fancy-serif-bold">Matt Lehrer</span>
@@ -9,10 +9,11 @@
 					An inquisitive American web developer, dad, and former finance strategist in Malmö,
 					Sweden.
 				</p>
-				<p class="mt-4 flex gap-4 text-lg font-thin sm:text-2xl">
+				<p class="mt-4 flex flex-wrap gap-4 text-lg font-thin sm:text-2xl">
 					<a href="/about">About</a>
 					<a href="/blog">Blog</a>
 					<a href="/reading/books">Book reviews</a>
+					<a href="/podcast-listening">Podcast recommendations</a>
 					<a href="/#projects">Portfolio</a>
 				</p>
 			</div>
