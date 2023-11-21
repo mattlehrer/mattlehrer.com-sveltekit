ed489e892fce30e866e75f48605b4c78fe754629
Matt Lehrer <lehrerm@gmail.com>
2023-11-21 15:35:35 +0100

    add hover and focus states

--- src/lib/components/portfolio/cards/ProjectCard.svelte
+++ src/lib/components/portfolio/cards/ProjectCard.svelte
@@ -9,29 +9,34 @@
 	export let href: string;
 </script>
 
-<article
-	class="@4xl:w-72 @4xl:px-4 @4xl:py-4 @sm:w-96 flex w-full flex-col rounded-sm bg-primary-200 px-6
-	py-6 shadow-inner"
->
-	<a href="/projects/#{id}">
-		<div class="mb-2 flex items-center justify-between">
-			<div class=" flex items-center gap-3">
-				<Img src={image} alt="{title} logo" class="h-6 w-6 rounded-sm shadow" />
-				<h3 class="text-xl font-bold">
-					{title}
-				</h3>
+<article class="rounded bg-primary-200 hover:bg-primary-400">
+	<a
+		href="/projects/#{id}"
+		class="block h-full rounded focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
+	>
+		<div
+			class="@4xl:w-72 @4xl:px-4 @4xl:py-4 @sm:w-96 flex w-full flex-col rounded
+	px-6 py-6 shadow-inner"
+		>
+			<div class="mb-2 flex items-center justify-between">
+				<div class=" flex items-center gap-3">
+					<Img src={image} alt="{title} logo" class="h-6 w-6 rounded-sm shadow" />
+					<h3 class="text-xl font-bold">
+						{title}
+					</h3>
+				</div>
+				<a
+					target="_blank"
+					{href}
+					class="rounded p-1 hover:bg-primary-800 hover:text-primary-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
+				>
+					<ExternalLink class="inline-block h-5 w-5 opacity-60" />
+				</a>
 			</div>
-			<a
-				target="_blank"
-				{href}
-				class="rounded-lg p-1 hover:bg-primary-800 hover:text-primary-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
-			>
-				<ExternalLink class="inline-block h-5 w-5 opacity-60" />
-			</a>
+			<p class="w-full">
+				<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+				{@html description}
+			</p>
 		</div>
-		<p class="w-full">
-			<!-- eslint-disable-next-line svelte/no-at-html-tags -->
-			{@html description}
-		</p>
 	</a>
 </article>
