eecf8fc440344d7082e6fa543e8597cd4754d4ac
Matt Lehrer <lehrerm@gmail.com>
2023-11-22 13:21:40 +0100

    add head

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
 	import Projects from '$lib/components/portfolio/Projects.svelte';
+	import { url } from '$lib/config';
 </script>
+
+<svelte:head>
+	<title>Matt Lehrer's web development projects and portfolio</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content="Matt Lehrer's web development projects and portfolio" />
+	<meta
+		content="{url}api/og?title=Matt Lehrer's web development projects and portfolio"
+		property="og:image"
+	/>
+	<meta content="Matt Lehrer's web development projects and portfolio" property="og:image:alt" />
+</svelte:head>
 
 <Projects />
