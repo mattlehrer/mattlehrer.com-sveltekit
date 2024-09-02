b96bfc83aa28a4f4a310fc5785c472e386a91741
Matt Lehrer <lehrerm@gmail.com>
2024-09-02 10:03:07 +0200

    update packages

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,12 @@
 import { escapeSvelte } from 'mdsvex';
 import remarkUnwrapImages from 'remark-unwrap-images';
 import rehypeSlug from 'rehype-slug';
-import { getHighlighter } from 'shiki';
+import {  createHighlighter } from 'shiki';
+
+const highlighter = await createHighlighter({
+	themes: ['night-owl'],
+	langs: ['javascript', 'svelte', 'typescript', 'html', 'css', 'json', 'markdown', 'zsh'],
+});
 
 /** @type {import('mdsvex').MdsvexOptions} */
 export const mdsvexOptions = {
@@ -14,10 +19,6 @@ export const mdsvexOptions = {
 	rehypePlugins: [rehypeSlug],
 	highlight: {
 		highlighter: async (code, lang = 'text') => {
-			const highlighter = await getHighlighter({
-				themes: ['night-owl'],
-				langs: ['javascript', 'svelte', 'typescript', 'html', 'css', 'json', 'markdown', 'zsh'],
-			});
 			const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'night-owl' }));
 			return `{@html \`${html}\` }`;
 		},
